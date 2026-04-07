1278bb67f1fb7fa10aa8b5b61d987769f10df214
INTERN10-LMANZANERO <intern10-lmanzanero@INTERN10-LMANZANERO.local>
2026-04-07 16:08:13 +0800

    Contacts Page

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,27 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:interfaces/pages/contact_page.dart';
 import 'pages/home_page.dart';
 import 'pages/about_page.dart';
 
-void main(){
+void main() {
   runApp(InternsProfile());
 }
 
-class InternsProfile extends StatelessWidget{
+class InternsProfile extends StatelessWidget {
   const InternsProfile({super.key});
   @override
-
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.black,
-      ),
+      theme: ThemeData(scaffoldBackgroundColor: Colors.black),
       title: 'Intern`s Profile',
       initialRoute: '/',
 
       routes: {
-        '/' : (context) => HomePage(),
-        '/about' : (context) => AboutPage(),
+        '/': (context) => HomePage(),
+        '/about': (context) => AboutPage(),
+        '/contacts': (context) => ContactPage(),
       },
     );
   }
--- frontend/lib/widgets/Homepage-Widgets/navbar.dart
+++ frontend/lib/widgets/Homepage-Widgets/navbar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:interfaces/pages/contact_page.dart';
 import 'package:interfaces/pages/home_page.dart';
 import '../../pages/about_page.dart';
 
@@ -13,71 +14,87 @@ class NavBar extends StatelessWidget implements PreferredSizeWidget {
       backgroundColor: Colors.black,
       toolbarHeight: 95,
       scrolledUnderElevation: 0,
-      title: Padding(padding: EdgeInsetsGeometry.only(left: 50, right: 50),
+      title: Padding(
+        padding: EdgeInsetsGeometry.only(left: 50, right: 50),
         child: Row(
           children: [
-            TextButton(onPressed: (){
-              Navigator.push(
-              context,
-              PageRouteBuilder(
-                pageBuilder: (context, animation1, animation2) => HomePage(),
-                transitionDuration: Duration.zero,
-                reverseTransitionDuration: Duration.zero,
-                ),
-              );
-            }, child: Text('Home', 
-              style: TextStyle(
-                fontSize: 12,
-                color: Colors.white,
-                ),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  PageRouteBuilder(
+                    pageBuilder: (context, animation1, animation2) =>
+                        HomePage(),
+                    transitionDuration: Duration.zero,
+                    reverseTransitionDuration: Duration.zero,
+                  ),
+                );
+              },
+              child: Text(
+                'Home',
+                style: TextStyle(fontSize: 12, color: Colors.white),
               ),
             ),
-            SizedBox(width: 20,),
-            TextButton(onPressed: (){
-              Navigator.push(
-              context,
-              PageRouteBuilder(
-                pageBuilder: (context, animation1, animation2) => AboutPage(),
-                transitionDuration: Duration.zero, // remove animation
-                reverseTransitionDuration: Duration.zero,
-              ),
-            );
-            }, child: Text('About', style: TextStyle(
-              color: Colors.white,
-              fontSize: 12,
-                ),
+            SizedBox(width: 20),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  PageRouteBuilder(
+                    pageBuilder: (context, animation1, animation2) =>
+                        AboutPage(),
+                    transitionDuration: Duration.zero, // remove animation
+                    reverseTransitionDuration: Duration.zero,
+                  ),
+                );
+              },
+              child: Text(
+                'About',
+                style: TextStyle(color: Colors.white, fontSize: 12),
               ),
             ),
-            SizedBox(width: 20,),
-            TextButton(onPressed: (){
-            }, 
-            child: Text('Contact',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 12,
-                ),
+            SizedBox(width: 20),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  PageRouteBuilder(
+                    pageBuilder: (context, animation1, animation2) =>
+                        ContactPage(),
+                    transitionDuration: Duration.zero,
+                    reverseTransitionDuration: Duration.zero,
+                  ),
+                );
+              },
+              child: Text(
+                'Contact',
+                style: TextStyle(color: Colors.white, fontSize: 12),
               ),
             ),
             Spacer(),
-            TextButton(onPressed: (){}, child: Text('Login', 
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 12,
-                ),
-              )
+            TextButton(
+              onPressed: () {},
+              child: Text(
+                'Login',
+                style: TextStyle(color: Colors.white, fontSize: 12),
+              ),
             ),
-            SizedBox(width: 20,),
-            TextButton(onPressed: (){}, child: Text('Register',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 12,
-            ),)),
-            SizedBox(width: 20,),
-            IconButton(onPressed: (){}, icon: Icon(Icons.light_mode, color: Colors.white,
-            size: 20,))
+            SizedBox(width: 20),
+            TextButton(
+              onPressed: () {},
+              child: Text(
+                'Register',
+                style: TextStyle(color: Colors.white, fontSize: 12),
+              ),
+            ),
+            SizedBox(width: 20),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.light_mode, color: Colors.white, size: 20),
+            ),
           ],
         ),
-      )
+      ),
     );
   }
 }
