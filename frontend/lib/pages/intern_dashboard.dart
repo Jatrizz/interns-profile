9d95b24c8f3915aacfa7e345c529412a59d986e3
INTERN7-JPUJEDA <fdsap-intern3@INTERN7-JPUJEDA.local>
2026-04-16 16:17:29 +0800

    Change int user_Id to String

--- frontend/lib/pages/intern_dashboard.dart
+++ frontend/lib/pages/intern_dashboard.dart
@@ -19,7 +19,7 @@ const String _base = 'http://127.0.0.1:8080';
 
 class InternDashboardPage extends StatefulWidget {
   final String firstName;
-  final int userId;
+  final String userId;
 
   const InternDashboardPage({
     super.key,
